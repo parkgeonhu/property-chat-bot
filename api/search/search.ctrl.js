1487d8de279da5efd7a79d28ca11116511c2aa60
parkgeonhu <parkgeonhu@gmail.com>
2020-10-23 02:41:30 +0900

    Add getDuration for calc time to arrive destination

--- api/search/search.ctrl.js
+++ api/search/search.ctrl.js
@@ -2,6 +2,8 @@ import db from '../../models'
 import * as sampleRequest from './expect_request.json'
 import * as answerInfo from '../../data/answer.info.json'
 import * as duration from '../../lib/parsing/duration'
+import * as util from '../../util/index'
+import { getKeywordInfo } from '../../lib/kakaoLocal'
 import { isSatisfySC } from '../../lib/surroundingInfo';
 const { Op } = require("sequelize");
 
@@ -70,7 +72,7 @@ const parsingParams = (params) => {
 
 const getWHERESequelize = (whereCondition) => {
     let result = {}
-    let exceptConditions = ['deposit', 'monthly_rent','deal_type']
+    let exceptConditions = ['deposit', 'monthly_rent', 'deal_type']
     for (let conditionKey of Object.keys(whereCondition)) {
         // let [key, value] = Object.entries(whereCondition[conditionKey])[0];
         if (exceptConditions.find(el => el == conditionKey) == undefined) {
@@ -78,7 +80,7 @@ const getWHERESequelize = (whereCondition) => {
         }
     }
 
-    
+
     result[`$Sales.deposit$`] = { [Op.lte]: whereCondition['deposit'] }
     result[`$Sales.monthly_rent$`] = { [Op.lte]: whereCondition['monthly_rent'] }
     result[`$Sales.deal_type$`] = whereCondition['deal_type']
@@ -99,23 +101,114 @@ const getWHERESequelize = (whereCondition) => {
     return result;
 }
 
+const getDestinationInfo = async (commute_location) => {
+    const data = await getKeywordInfo(commute_location)
+    const isValid = data.meta.total_count > 0 ? true : false; // 검색 결과가 0 초과면 valid 하다고 판단
+
+    if (isValid) {
+        // 아파트만 파싱하도록.
+        const document = data.documents[0]
+        console.log(document)
+        const x = document.x;
+        const y = document.y;
+
+        return {
+            is_valid: true,
+            x,
+            y
+        }
+    }
+    else {
+        return {
+            is_valid: false
+        }
+    }
+}
+
+const getDuration = async (userCondition, sales) => {
+    const commute_type = userCondition["commute_type"]
+    const commute_time = userCondition["commute_time"]
+    const commute_location = userCondition["commute_location"]
+
+    console.log(commute_location)
+
+    const destination = await getDestinationInfo(commute_location)
+    if (destination['is_valid'] == false) {
+        //통근 주소가 잘못됐다는 반려폼 들어가야함
+        return {
+
+        }
+    }
+
+    console.log(destination)
+
+    const dx = parseFloat(destination['x']);
+    const dy = parseFloat(destination['y']);
+
+    let sales_sorted_by_distance = sales.map(sale => {
+        console.log(sale['x'], sale['y'], dx, dy)
+        const distance = util.getDistance(sale['x'], sale['y'], dx, dy)
+        return {
+            ...sale,
+            distance
+        }
+        sale['distance'] = distance
+    })
+
+    sales_sorted_by_distance.sort((a, b) => {
+        if(a.distance < b.distance){
+            return -1;
+        }else if(a.distance > b.distance){
+            return 1;
+        } 
+        return 0;
+    })
+
+    console.log(sales_sorted_by_distance)
+
+    let sales_sliced = sales_sorted_by_distance.slice(0,10)
+
+    let sales_sorted;
+    if (commute_type == 'transit') {
+        sales_sorted = await Promise.all(sales_sliced.map(async sale=>{
+            return {
+                sales_sorted_by_distance,
+                duration : await duration.getTransitDuration(sale['x'], sale['y'], dx, dy)
+            }
+        }))
+    } else if (commute_type == 'driving') {
+        sales_sorted = await Promise.all(sales_sliced.map(async sale=>{
+            return {
+                sales_sorted_by_distance,
+                duration : await duration.getDrivingDuration(sale['x'], sale['y'], dx, dy)
+            }
+        }))
+    }
+
+    util.writeJSONData("getDuration", sales_sorted)
+}
+
 
 export const test = async ctx => {
     const params = sampleRequest.action.params;
 
-    const conditions=parsingParams(params)
+    const conditions = parsingParams(params)
     console.log(conditions)
 
     console.log(conditions["whereCondition"])
-    const where=getWHERESequelize(conditions["whereCondition"])
+    const where = getWHERESequelize(conditions["whereCondition"])
 
     const result = await db.Apt.findAll({
         where,
+        raw:true,
         include: [{
             model: db.Sale,
             as: 'Sales'
         }]
     })
+
+    const userCondition = conditions["userCondition"]
+    await getDuration(userCondition, result)
 
     ctx.status = 200;
     ctx.body = JSON.stringify(result)
@@ -123,11 +216,11 @@ export const test = async ctx => {
 
 export const durationTest = async ctx => {
 
-    const driving = await duration.getDrivingDuration(127.07703045060357,37.63906582026493, 126.996969239236, 37.6107638961532)
-    const transit = await duration.getTransitDuration(127.07703045060357,37.63906582026493, 126.996969239236, 37.6107638961532)
+    const driving = await duration.getDrivingDuration(127.07703045060357, 37.63906582026493, 126.996969239236, 37.6107638961532)
+    const transit = await duration.getTransitDuration(127.07703045060357, 37.63906582026493, 126.996969239236, 37.6107638961532)
 
     console.log(driving, transit)
-    
+
     ctx.status = 200;
     ctx.body = {
         driving,
@@ -138,11 +231,11 @@ export const durationTest = async ctx => {
 
 export const getSchoolNumber = async ctx => {
 
-    console.log(await isSatisfySC(127.07703045060357,37.63906582026493))
-    
+    console.log(await isSatisfySC(127.07703045060357, 37.63906582026493))
+
     ctx.status = 200;
     ctx.body = {
-        "status" : "success"
+        "status": "success"
     }
 }
 
@@ -150,9 +243,9 @@ export const getSchoolNumber = async ctx => {
 export const getHashTag = async ctx => {
 
     const hastagExample = {
-        "민대인" : "뚝배기"
+        "민대인": "뚝배기"
     }
-    
+
     ctx.status = 200;
     ctx.body = hastagExample
 }
--- util/index.js
+++ util/index.js
@@ -1,23 +1,29 @@
 var fs = require('fs');
 
 export const writeJSONData = (funcName, data) => {
-    fs.writeFile(`./log/${funcName}.txt`, JSON.stringify(data), 'utf8', function (err) {
-        console.log(`${funcName}.txt 쓰기 완료`);
-    });
+  fs.writeFile(`./log/${funcName}.txt`, JSON.stringify(data), 'utf8', function (err) {
+    console.log(`${funcName}.txt 쓰기 완료`);
+  });
 }
 
 export const getUniqueArray = (array, key) => {
-    return array.filter((item, i) => {
-      return array.findIndex((item2, j) => {
-        return item[key] === item2[key];
-      }) === i;
-    });
+  return array.filter((item, i) => {
+    return array.findIndex((item2, j) => {
+      return item[key] === item2[key];
+    }) === i;
+  });
 }
 
 export const getRefinedPrice = (str) => {
-    let temp1 = str.trim();
-    let temp2 = temp1.replace(',', '');
-    let int_deposit = parseInt(temp2);
+  let temp1 = str.trim();
+  let temp2 = temp1.replace(',', '');
+  let int_deposit = parseInt(temp2);
 
-    return int_deposit;
+  return int_deposit;
+}
+
+export const getDistance = (sx, sy, dx, dy) => {
+  const diffX = dx - sx;
+  const diffY = dy - sy;
+  return Math.sqrt(Math.abs(diffX * diffX) + Math.abs(diffY * diffY));
 }
